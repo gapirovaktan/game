0d0f291896f58df670eee14531b0e7bfcfa55691
gapirovaktan <150505604+gapirovaktan@users.noreply.github.com>
2023-12-26 21:30:07 +0600

    Update MEMORY GAME.cpp

--- memory game/MEMORY GAME.cpp
+++ memory game/MEMORY GAME.cpp
@@ -6,7 +6,7 @@ int main() {
     int matched = 0;
     int moves = 0;
     cout << "Welcome to the Memory Game" << endl;
-    cout << "Enter the size of an array (6-20): ";
+    cout << "Введите размер массива (6-15): ";
     cin >> a;
 
     int arr[a];
@@ -21,24 +21,22 @@ int main() {
         numbers[i] = "?";
     }
 
-    random_device rd;
-    mt19937 g(rd());
-    shuffle(arr, arr + a, g);
+   
 
     while (matched == 0) {
         int index1, index2;
         int check = 0;
-        cout << "Here is the array: ";
+        cout << "Вот массив: ";
         for (int i = 0; i < a; i++) {
             cout << numbers[i] << " ";
         }
         cout << endl;
-        cout << "Enter the index to show: ";
+        cout << "Введите индексы, чтобы показать: ";
         cin >> index1;
-        cout << "The card at the index " << index1 << " is: " << arr[index1] << endl;
+        cout << "Карта по индексу " << index1 << " is: " << arr[index1] << endl;
         cout << "Enter the second index to show: ";
         cin >> index2;
-        cout << "The card at the index " << index2 << " is: " << arr[index2] << endl;
+        cout << " Карта по индексу" << index2 << " is: " << arr[index2] << endl;
         moves++;
         for (int i = 0; i < a; i++) {
             if (i == index1 or i == index2) {
@@ -49,29 +47,10 @@ int main() {
             }
         }
         if (arr[index1] == arr[index2]) {
-            cout << "\nGreat the cards are matched. Continue..." << endl;
+            cout << "\nОтлично, карты совпали. Продолжать..." << endl;
             numbers[index1] = to_string(arr[index1]);
             numbers[index2] = to_string(arr[index2]);
-        }
-        else {
-            cout << "\nThe cards do not match! Try again!" << endl;
-        }
-        for (int i = 0; i < a; i++) {
-            if (numbers[i] != "?") {
-                check++;
-            }
-        }
-        if (check == a) {
-            cout << "You won!"<<endl;
-            cout<<"Total moves: " << moves << endl;
-            matched = 1;
-        }
-        else {
-            cout << "Press Enter to continue...";
-            cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            cin.get();
-            system("cls");
-        }
+        
+       
     }
 }
